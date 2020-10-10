47f8ac2601321ea74dfaeac65c8ea40a5aec1eea
rmazzine <mazzine.r@gmail.com>
2020-10-10 15:24:07 +0200

    Add linting configuration
    Additionally to this, the VS Code must be modified in the settings ~/.config/Code/User/settings.json to add ("editor.formatOnSave": true, "[javascript]": { "editor.formatOnSave": false, }, "eslint.autoFixOnSave": true, "eslint.alwaysShowStatus": true). It also needed to instal eslint pluggin.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-const App = () =>{
-    return <div>Hi there!</div>;
-}
+const App = () => <div>Hi there!</div>;
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector("#root")  
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
