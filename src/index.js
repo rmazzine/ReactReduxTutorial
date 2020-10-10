d36fb66fd4daf099e43a499f92af20ed740a4631
rmazzine <mazzine.r@gmail.com>
2020-10-10 18:05:20 +0200

    Add component as a props children

--- src/index.js
+++ src/index.js
@@ -4,28 +4,35 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatarImage={faker.image.avatar()}
-        commentText={faker.lorem.lines(2)}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        avatarImage={faker.image.avatar()}
-        commentText={faker.lorem.lines(2)}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00AM"
-        avatarImage={faker.image.avatar()}
-        commentText={faker.lorem.lines(2)}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatarImage={faker.image.avatar()}
+          commentText={faker.lorem.lines(2)}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatarImage={faker.image.avatar()}
+          commentText={faker.lorem.lines(2)}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00AM"
+          avatarImage={faker.image.avatar()}
+          commentText={faker.lorem.lines(2)}
+        />
+      </ApprovalCard>
     </div>
   );
 };
