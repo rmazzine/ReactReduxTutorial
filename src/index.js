7d872038ed674dcae59ae9a5c29fc293359adfe3
rmazzine <mazzine.r@gmail.com>
2020-10-10 17:43:40 +0200

    Multiple props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import faker from 'faker';
+
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        avatarImage={faker.image.avatar()}
+        commentText={faker.lorem.lines(2)}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        avatarImage={faker.image.avatar()}
+        commentText={faker.lorem.lines(2)}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00AM"
+        avatarImage={faker.image.avatar()}
+        commentText={faker.lorem.lines(2)}
+      />
     </div>
   );
 };
