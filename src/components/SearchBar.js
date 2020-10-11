7eccd01c0977f502cec3fc6645914d85ed97182d
rmazzine <mazzine.r@gmail.com>
2020-10-11 16:59:42 +0200

    Handling context problems

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  state = {term: ''};
+  constructor(props) {
+    super(props);
 
+    this.state = { term: '' };
+
+    // Needed to the function "know" the context
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(this.state.term);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             {/* The parentesis off will call the function every time the event onChange is called */}
-            <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value})} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
