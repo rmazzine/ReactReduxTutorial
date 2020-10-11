ef5fbaf777a1d290e3822feaad30b5cd27c82821
rmazzine <mazzine.r@gmail.com>
2020-10-11 12:00:01 +0200

    Create simple component with state

--- src/index.js
+++ src/index.js
@@ -2,19 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a react component
-const App = () => {
-  const buttonText = 'Click me!';
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props); // Because we override the constructor class by extending, then it must get the parent props
+
+    // This is the only time we do direct assignment
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      // To change state, always use setState!!
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => console.log(err)
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    // Avoid include run code as this will run all time
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
